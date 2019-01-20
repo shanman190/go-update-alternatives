18d1f6f6e1555f1443a57bad0ba03278e2121b22
Shannon Pamperl <shanman190@gmail.com>
2019-01-19 21:36:56 -0600

    Sync error output with Linux version for install command

--- commands/install.go
+++ commands/install.go
@@ -22,22 +22,34 @@ func (command *InstallCommand) Execute(args []string) error {
 	group := args[1]
 	path := filepath.Clean(args[2])
 
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		fmt.Fprintf(ui.Stderr, "update-alternatives: error: alternative path %s doesn't exist\n", path)
+		os.Exit(1)
+	} else if err != nil {
+		fmt.Fprintln(ui.Stderr, "update-alternatives: error: unknown error encountered")
+		os.Exit(1)
+	}
+
 	alternativesDir := config.GetAlternativesDir()
 	if err := os.MkdirAll(alternativesDir, os.ModePerm); err != nil {
-		fmt.Fprintf(ui.Stderr, "Error: unable to create directory '%s'\n", alternativesDir)
+		fmt.Fprintf(ui.Stderr, "update-alternatives: error: unable to create alternatives directory '%s'\n", alternativesDir)
 		os.Exit(1)
 	}
-	config.SaveAlternative(group, path)
+
 	alternativePath := filepath.Join(alternativesDir, group)
 
-	err := symbolic.Ln(path, alternativePath)
-	if err != nil {
-		fmt.Fprintf(ui.Stderr, "Error: Unable to create symboic link from %s to %s\n\n%s\n", path, alternativePath, err)
+	if _, err := os.Stat(alternativePath); os.IsNotExist(err) {
+		fmt.Printf("update-alternatives: using %s to provide %s (%s)\n", path, link, group)
+	}
+
+	config.SaveAlternative(group, path)
+
+	if err := symbolic.Ln(path, alternativePath); err != nil {
+		fmt.Fprintf(ui.Stderr, "update-alternatives: error: unable to install '%s' to '%s': %s\n", path, alternativePath, err)
 		os.Exit(1)
 	}
-	err = symbolic.Ln(alternativePath, link)
-	if err != nil {
-		fmt.Fprintf(ui.Stderr, "Error: Unable to create symbolic link from %s to %s\n\n%s\n", alternativePath, link, err)
+	if err := symbolic.Ln(alternativePath, link); err != nil {
+		fmt.Fprintf(ui.Stderr, "update-alternatives: error: unable to install '%s' to '%s': %s\n", alternativePath, link, err)
 		os.Exit(1)
 	}
 	
